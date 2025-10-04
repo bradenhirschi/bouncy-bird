cf2a186b3f7326f4791195f55a49f4dc5e7bc110
Braden Hirschi <bradenhirschi29@gmail.com>
2025-10-04 11:24:53 -0500

    Add background

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "bn_core.h"
+#include "bn_regular_bg_items_background.h"
+#include "bn_regular_bg_ptr.h"
 #include "bn_unique_ptr.h"
 
 #include "bb_game_state.h"
@@ -7,6 +9,10 @@
 int main() {
     bn::core::init();
 
+    // Draw background which is the same for all game states
+    bn::regular_bg_ptr background = bn::regular_bg_items::background.create_bg(0, 0);
+
+    // Initialize game state
     bn::unique_ptr<bb::GameState> current_state = bn::make_unique<bb::MainState>();
 
     while (true) {
